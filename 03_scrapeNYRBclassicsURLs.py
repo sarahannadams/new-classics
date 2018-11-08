8dd35e7198c3b5506d3fdec55dbc058dc9f075c9
sarahannadams <s,ann.adams5761@gmail.com>
2018-11-07 20:25:24 -0500

    Scraping to retrieve translation, ISBN, and pub date

--- 03_scrapeNYRBclassicsURLs.py
+++ 03_scrapeNYRBclassicsURLs.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import requests, json, time, re, csv
+import requests, json, time, re, csv, time
 
 def auth_translation_clean(str_input):
 
@@ -8,7 +8,10 @@ def auth_translation_clean(str_input):
 
 	return str_input
 
+#create empty list for books
 book_info_list = []
+
+#create empty list for book urls
 url_list = []
 
 with open('new_classics.csv','r', encoding="utf-8") as f:
@@ -21,9 +24,11 @@ with open('new_classics.csv','r', encoding="utf-8") as f:
 		# how do we assign the rows to be the url variable used in the get request?
 		url_list.append(row[3])
 
+count = 0
+
 for url in url_list:
 
-	# print("scraping!" + url)
+	print("scraping!" + url + " " + str(count))
 	nyrb_classics_book = requests.get(url)
 
 	#setting the variable for the HTML of the page
@@ -35,70 +40,67 @@ for url in url_list:
 	#identifying the 1st parent div that holds the elements we want to scrape
 	all_info = soup.find_all("div", attrs={"class":"span8"})
 
-	count=0
-
 	#looping through to pull out the elements we want
 	for item in all_info:
 
-		if item == None:
-			continue
+		# #subtitle is under the span element class=subtitle
+		# span_subtitle = item.find("span", attrs={"class": "subtitle"})
+		# subtitle = span_subtitle.text
 
-			#subtitle is under the span element class=subtitle
-			span_subtitle = item.find("span", attrs={"class": "subtitle"})
-			subtitle = span_subtitle.text
+		# # print(subtitle)
 
-			# print(subtitle)
+		#author and translation information is under the h2 element class=combined-authors
+		h2_combined_authors = item.find("h2", attrs={"class": "combined-authors"})
+		authors_translation = auth_translation_clean(h2_combined_authors.text)
 
-			#author and translation information is under the h2 element class=combined-authors
-			h2_combined_authors = item.find("h2", attrs={"class": "combined-authors"})
-			authors_translation = auth_translation_clean(h2_combined_authors.text)
+		# print(authors_translation)
 
-			# print(authors_translation())
+		#identifying the 2nd parent div that holds the elements we want to scrape
+		all_info_2 = soup.find_all("div", attrs={"class":"description additional"})
 
-			#identifying the 2nd parent div that holds the elements we want to scrape
-			all_info_2 = soup.find_all("div", attrs={"class":"description additional"})
+		#looping through to pull out the elements we want
+		for item in all_info_2:
 
-			#looping through to pull out the elements we want
-			for item in all_info_2:
+			#isbn is under the span element class=variant-sku
+			span_variant_sku = item.find("span", attrs={"class": "variant-sku"})
+			isbn = span_variant_sku.text
 
-				#isbn is under the span element class=variant-sku
-				span_variant_sku = item.find("span", attrs={"class": "variant-sku"})
-				isbn = span_variant_sku.text
+			# print(isbn.strip())
 
-				# print(isbn.strip())
+			#publication date is found wihtin additional info, which is under the span p element 
+			p_addl_info = item.find("p")
+			
+			#created a regex to target just the publication date
+			x = re.compile('[A-Z][a-z]+ [0-9]+[,] [0-9]+')
+			pub_date_x = x.findall(p_addl_info.text)
 
-				#publication date is found wihtin additional info, which is under the span p element 
-				p_addl_info = item.find("p")
-				
-				#created a regex to target just the publication date
-				x = re.compile('[A-Z][a-z]+ [0-9]+[,] [0-9]+')
-				pub_date_x = x.findall(p_addl_info.text)
+			#the regex returns the date as a list with only one item. this next line of code targets the list item, rather than the list
+			pub_date = pub_date_x[0]
 
-				#the regex returns the date as a list with only one item. this next line of code targets the list item, rather than the list
-				pub_date = pub_date_x[0]
+			# print(str(pub_date))
 
-				# print(str(pub_date))
+			#adding a count variable so that it counts each loop
+			count = count + 1
 
-				#adding a count variable so that it counts each loop
-				count = count + 1
+	#setting up dictionary 
+	new_classics_book_info = {}
 
-				#setting up dictionary 
-				new_classics_book_info = {}
+	#defining keys and values in dictionary
+	new_classics_book_info["id"] = count
+	# new_classics_book_info["subtitle"] = subtitle
+	new_classics_book_info["author_translation_note"] = authors_translation
+	new_classics_book_info["isbn"] = isbn
+	new_classics_book_info["pub_date"] = pub_date		
 
-				#defining keys and values in dictionary
-				new_classics_book_info["id"] = count
-				new_classics_book_info["subtitle"] = subtitle
-				new_classics_book_info["author_translation_note"] = authors_translation
-				new_classics_book_info["isbn"] = isbn
-				new_classics_book_info["pub_date"] = pub_date		
+	#appending our dictionary to our list of books
+	book_info_list.append(new_classics_book_info)
 
-				#appending our dictionary to our list of books
-				book_info_list.append(new_classics_book_info)
+	# print(book_info_list)
 
-			# print(book_info_list)
+	time.sleep(3)
 
-			#dumping our list of dictionaries into a JSON file
-			json.dump(book_info_list, open("new_classics_book_info.json", "w"), indent=2)
+#dumping our list of dictionaries into a JSON file
+json.dump(book_info_list, open("new_classics_book_info.json", "w"), indent=2)
 
-		#prints the number of titles to command line (not included in JSON file)	
-		print(len(book_info_list))
+#prints the number of titles to command line (not included in JSON file)	
+print(len(book_info_list))
