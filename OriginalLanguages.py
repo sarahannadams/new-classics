8eb8a5af0f0231257bd20785f9e686b261515455
Alice Griffin <alg2179@gmail.com>
2018-11-07 20:22:59 -0500

    trying to English

--- OriginalLanguages.py
+++ OriginalLanguages.py
@@ -3,11 +3,13 @@ import json, csv, re
 #opening the json file with the information from each book page
 book_info = json.load(open("new_classics_book_info.json", "r"))
 
-#setting up a list 
+#setting up a list from which I am going to form my JSON (list of dictionaries later)
 language_list = []
 
+#setting up count which will be the ID later
 count=0
 
+#function 
 def language_pull_clean(input):
 
 	input = str(input)
@@ -18,14 +20,16 @@ def language_pull_clean(input):
 for orig_language in book_info:
 
 	find_language = re.compile("(?<=from the) [A-Z][a-z]+")
-	orig_language = find_language.findall(str(book_info))
+	orig_language = find_language.findall(str(orig_language))
 	orig_language = language_pull_clean(orig_language)
+	#english = str("English")
 
 	if find_language == re.compile("(?<=from the) [A-Z][a-z]+"):
 		language = orig_language
-	
+
 	else:
-		language = "English"
+		language == "English"
+		
 	
 	count= count + 1
 		
@@ -33,10 +37,14 @@ for orig_language in book_info:
 	
 	orig_language_info["id"] = count
 	orig_language_info["original_language"] = language
+# 	orig_language_info["English"] = language2
 	
 	language_list.append(orig_language_info)
 	
 json.dump(language_list, open("language_list.json", "w"), indent=2)
 	
+	
+
+
 	
 
